5e227b960bdfb5374a2bc4f6c8fc537e1561e434
shreyanshu005 <shreyanshu005@gmail.com>
2025-06-25 23:36:51 +0530

    updated

--- auth-system/src/index.ts
+++ auth-system/src/index.ts
@@ -24,7 +24,10 @@ app.use(cors({
     'http://localhost:3000',
     'http://localhost:3001',
     'http://localhost:3002',
-    'https://ias-frontend-snowy.vercel.app'
+    'https://ias-frontend-snowy.vercel.app',
+    'https://www.maheshanias.com',
+    'https://maheshanias.com'
+
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
